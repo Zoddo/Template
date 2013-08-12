78bc42c378c40e3f12fab43c3c8babf919addacc
Zoddo <zoddo.ino@gmail.com>
2013-08-12 19:56:53 +0200

    Create simple.php

---
+++ example/simple.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'template.class.php';
+
+$tpl = new Template('test', './tpl/');
+
+$tpl->assign_vars(array(
+	'TEST_VAR'	=> '$value',
+	'NUM'		=> '°',
+	'TEST'		=> true,
+	'TIME'		=> time(),
+));
+
+$tpl->block_assign_vars('block', array(
+	'TEST1'	=> 1,
+	'TEST2'	=> 2,
+));
+
+$tpl->block_assign_vars('block', array(
+	'TEST1'	=> 5,
+	'TEST2'	=> 3,
+));
+
+$tpl->set_name(array(
+	'test'	=> 'test.html',
+));
+
+$tpl->display('test');
