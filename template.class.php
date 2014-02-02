73bdeff8cf0598513eb55d5fe0fecf940142f9ce
Zoddo <zoddo.ino@gmail.com>
2014-02-02 18:04:47 +0100

    Added support for sub-loops

--- template.class.php
+++ template.class.php
@@ -12,6 +12,8 @@ class Template
 	protected $_vars = array();
 	protected $_bvars = array();
 	protected $_files = array();
+	
+	protected $block_names = array();
 
 	// Constructor
 	public function __construct($name, $dir)
@@ -47,12 +49,63 @@ class Template
 	// Assign a block of variables in the template system
 	public function block_assign_vars($block, array $vars)
 	{
-		if(!isset($this->_bvars[$block]))
+		if (strpos($block, '.') !== false)
 		{
-			$this->_bvars[$block] = array();
-		}
+			// Nested block.
+			$blocks = explode('.', $block);
+			$blockcount = sizeof($blocks) - 1;
 
-		$this->_bvars[$block][] = $vars;
+			$str = &$this->_tpldata;
+			for ($i = 0; $i < $blockcount; $i++)
+			{
+				$str = &$str[$blocks[$i]];
+				$str = &$str[sizeof($str) - 1];
+			}
+
+			$s_row_count = isset($str[$blocks[$blockcount]]) ? sizeof($str[$blocks[$blockcount]]) : 0;
+			$vars['S_ROW_COUNT'] = $s_row_count;
+
+			// Assign S_FIRST_ROW
+			if (!$s_row_count)
+			{
+				$vars['S_FIRST_ROW'] = true;
+			}
+
+			// Now the tricky part, we always assign S_LAST_ROW and remove the entry before
+			// This is much more clever than going through the complete template data on display (phew)
+			$vars['S_LAST_ROW'] = true;
+			if ($s_row_count > 0)
+			{
+				unset($str[$blocks[$blockcount]][($s_row_count - 1)]['S_LAST_ROW']);
+			}
+
+			// Now we add the block that we're actually assigning to.
+			// We're adding a new iteration to this block with the given
+			// variable assignments.
+			$str[$blocks[$blockcount]][] = $vars;
+		}
+		else
+		{
+			// Top-level block.
+			$s_row_count = (isset($this->_tpldata[$block])) ? sizeof($this->_tpldata[$block]) : 0;
+			$vars['S_ROW_COUNT'] = $s_row_count;
+
+			// Assign S_FIRST_ROW
+			if (!$s_row_count)
+			{
+				$vars['S_FIRST_ROW'] = true;
+			}
+
+			// We always assign S_LAST_ROW and remove the entry before
+			$vars['S_LAST_ROW'] = true;
+			if ($s_row_count > 0)
+			{
+				unset($this->_tpldata[$block][($s_row_count - 1)]['S_LAST_ROW']);
+			}
+
+			// Add a new iteration to this block with the variable assignments we were given.
+			$this->_tpldata[$block][] = $vars;
+		}
 
 		return true;
 	}
@@ -123,7 +176,7 @@ class Template
 		$tpl_data = preg_replace('#<!-- ELSE -->#U', '<?php }else{?>', $tpl_data);
 		$tpl_data = preg_replace('#<!-- ENDIF -->#U', '<?php }?>', $tpl_data);
 
-		$tpl_data = preg_replace('#<!-- BEGIN ([a-zA-Z0-9._-]+) -->#U', '<?php $_$1_count=(isset($this->_bvars[\'$1\'])) ? sizeof($this->_bvars[\'$1\']) : 0;if ($_$1_count){for($_$1_i = 0; $_$1_i < $_$1_count; ++$_$1_i){$_$1_val=$this->_bvars[\'$1\'][$_$1_i];?>', $tpl_data);
+		$tpl_data = preg_replace_callback('#<!-- BEGIN ([a-zA-Z0-9._-]+) -->#U', array($this, '_tpl_compile_begin'), $tpl_data);
 		$tpl_data = preg_replace_callback('#<!-- BEGIN ?ELSE ?IF (.*?)? -->#', array($this, '_tpl_compile_beginelseif'), $tpl_data);
 		$tpl_data = preg_replace('#<!-- BEGIN ?ELSE -->#U', '<?php }}else{{?>', $tpl_data);
 		$tpl_data = preg_replace('#<!-- END ([a-zA-Z0-9._-]+) -->#U', '<?php }}?>', $tpl_data);
@@ -186,7 +239,7 @@ class Template
 	/*
 	 * Generates a reference to the given variable inside the given (possibly nested)
 	 * block namespace. This is a string of the form:
-	 * ' . $this->_tpldata['parent'][$_parent_i]['$child1'][$_child1_i]['$child2'][$_child2_i]...['varname'] . '
+	 * ' . $this->_bvars['parent'][$_parent_i]['$child1'][$_child1_i]['$child2'][$_child2_i]...['varname'] . '
 	 */
 	protected function _generate_block_varref($namespace, $varname, $echo = true, $defop = false)
 	{
@@ -363,6 +416,106 @@ class Template
 	protected function _tpl_compile_elseif($tag_args)
 	{
 		return $this->_tpl_compile_if($tag_args, true);
+	}
+	
+	protected function _tpl_compile_begin($tag_args)
+	{
+		$no_nesting = false;
+
+		// Is the designer wanting to call another loop in a loop?
+		if (strpos($tag_args, '!') === 0)
+		{
+			// Count the number of ! occurrences (not allowed in vars)
+			$no_nesting = substr_count($tag_args, '!');
+			$tag_args = substr($tag_args, $no_nesting);
+		}
+
+		// Allow for control of looping (indexes start from zero):
+		// foo(2)    : Will start the loop on the 3rd entry
+		// foo(-2)   : Will start the loop two entries from the end
+		// foo(3,4)  : Will start the loop on the fourth entry and end it on the fifth
+		// foo(3,-4) : Will start the loop on the fourth entry and end it four from last
+		if (preg_match('#^([^()]*)\(([\-\d]+)(?:,([\-\d]+))?\)$#', $tag_args, $match))
+		{
+			$tag_args = $match[1];
+
+			if ($match[2] < 0)
+			{
+				$loop_start = '($_' . $tag_args . '_count ' . $match[2] . ' < 0 ? 0 : $_' . $tag_args . '_count ' . $match[2] . ')';
+			}
+			else
+			{
+				$loop_start = '($_' . $tag_args . '_count < ' . $match[2] . ' ? $_' . $tag_args . '_count : ' . $match[2] . ')';
+			}
+
+			if (strlen($match[3]) < 1 || $match[3] == -1)
+			{
+				$loop_end = '$_' . $tag_args . '_count';
+			}
+			else if ($match[3] >= 0)
+			{
+				$loop_end = '(' . ($match[3] + 1) . ' > $_' . $tag_args . '_count ? $_' . $tag_args . '_count : ' . ($match[3] + 1) . ')';
+			}
+			else //if ($match[3] < -1)
+			{
+				$loop_end = '$_' . $tag_args . '_count' . ($match[3] + 1);
+			}
+		}
+		else
+		{
+			$loop_start = 0;
+			$loop_end = '$_' . $tag_args . '_count';
+		}
+
+		$tag_template_php = '';
+		array_push($this->block_names, $tag_args);
+
+		if ($no_nesting !== false)
+		{
+			// We need to implode $no_nesting times from the end...
+			$block = array_slice($this->block_names, -$no_nesting);
+		}
+		else
+		{
+			$block = $this->block_names;
+		}
+
+		if (sizeof($block) < 2)
+		{
+			// Block is not nested.
+			$tag_template_php = '$_' . $tag_args . "_count = (isset(\$this->_bvars['$tag_args'])) ? sizeof(\$this->_bvars['$tag_args']) : 0;";
+			$varref = "\$this->_bvars['$tag_args']";
+		}
+		else
+		{
+			// This block is nested.
+			// Generate a namespace string for this block.
+			$namespace = implode('.', $block);
+
+			// Get a reference to the data array for this block that depends on the
+			// current indices of all parent blocks.
+			$varref = $this->_generate_block_data_ref($namespace, false);
+
+			// Create the for loop code to iterate over this block.
+			$tag_template_php = '$_' . $tag_args . '_count = (isset(' . $varref . ')) ? sizeof(' . $varref . ') : 0;';
+		}
+
+		$tag_template_php .= 'if ($_' . $tag_args . '_count) {';
+
+		/**
+		* The following uses foreach for iteration instead of a for loop, foreach is faster but requires PHP to make a copy of the contents of the array which uses more memory
+		* <code>
+		*	if (!$offset)
+		*	{
+		*		$tag_template_php .= 'foreach (' . $varref . ' as $_' . $tag_args . '_i => $_' . $tag_args . '_val){';
+		*	}
+		* </code>
+		*/
+
+		$tag_template_php .= 'for ($_' . $tag_args . '_i = ' . $loop_start . '; $_' . $tag_args . '_i < ' . $loop_end . '; ++$_' . $tag_args . '_i){';
+		$tag_template_php .= '$_'. $tag_args . '_val = &' . $varref . '[$_'. $tag_args. '_i];';
+
+		return $tag_template_php;
 	}
 
 	protected function _tpl_compile_beginelseif($tag_args)
